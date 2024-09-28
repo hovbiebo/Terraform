de0cb1bd94f7e80953d3b54eacd14684a5a2d4d6
Henry Ovbiebo <henry.ovbiebo@gmail.com>
2024-09-28 12:02:26 -0500

    9/28 changes and commit

--- vpc.tf
+++ vpc.tf
@@ -1,128 +1,128 @@
-# # create vpc
-# # terraform aws create vpc
-# resource "aws_vpc" "Training_vpc" {
-#   cidr_block              = var.vpc_cidr
-#   instance_tenancy        = "default"
-#   enable_dns_hostnames    = "false"
+# create vpc
+# terraform aws create vpc
+resource "aws_vpc" "Training_vpc" {
+  cidr_block              = var.vpc_cidr
+  instance_tenancy        = "default"
+  enable_dns_hostnames    = "false"
 
-#   tags      = {
-#     Name    = "Training_Main_VPC"
-#   }
-# }
+  tags      = {
+    Name    = "Training_Main_VPC"
+  }
+}
 
-# # create internet gateway and attach it to vpc
-# # terraform aws create internet gateway
-# resource "aws_internet_gateway" "internet_gateway" {
-#   vpc_id    = aws_vpc.Training_vpc.id
+# create internet gateway and attach it to vpc
+# terraform aws create internet gateway
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id    = aws_vpc.Training_vpc.id
 
-#   tags      = {
-#     Name    = "Training_Main_IGW"
-#   }
-# }
+  tags      = {
+    Name    = "Training_Main_IGW"
+  }
+}
 
-# # create public subnet az1
-# # terraform aws create subnet
-# resource "aws_subnet" "public_subnet_az1" {
-#   vpc_id                  = aws_vpc.Training_vpc.id
-#   cidr_block              = var.public_subnet_az1_cidr
-#   availability_zone       = var.Availability_Zone1
-#   map_public_ip_on_launch = "false"
+# create public subnet az1
+# terraform aws create subnet
+resource "aws_subnet" "public_subnet_az1" {
+  vpc_id                  = aws_vpc.Training_vpc.id
+  cidr_block              = var.public_subnet_az1_cidr
+  availability_zone       = var.Availability_Zone1
+  map_public_ip_on_launch = "false"
 
-#   tags      = {
-#     Name    = "Training_Main_PubSub1"
-#   }
-# }
+  tags      = {
+    Name    = "Training_Main_PubSub1"
+  }
+}
 
-# # create public subnet az2
-# # terraform aws create subnet
-# resource "aws_subnet" "public_subnet_az2" {
-#   vpc_id                  = aws_vpc.Training_vpc.id
-#   cidr_block              = var.public_subnet_az2_cidr
-#   availability_zone       = var.Availability_Zone2
-#   map_public_ip_on_launch = "false"
+# create public subnet az2
+# terraform aws create subnet
+resource "aws_subnet" "public_subnet_az2" {
+  vpc_id                  = aws_vpc.Training_vpc.id
+  cidr_block              = var.public_subnet_az2_cidr
+  availability_zone       = var.Availability_Zone2
+  map_public_ip_on_launch = "false"
 
-#   tags      = {
-#     Name    = "Training_Main_PubSub2"
-#   }
-# }
+  tags      = {
+    Name    = "Training_Main_PubSub2"
+  }
+}
 
-# # create route table and add public route
-# # terraform aws create route table
-# resource "aws_route_table" "public_route_table" {
-#   vpc_id       = aws_vpc.Training_vpc.id
+# create route table and add public route
+# terraform aws create route table
+resource "aws_route_table" "public_route_table" {
+  vpc_id       = aws_vpc.Training_vpc.id
 
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.internet_gateway.id
-#   }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
 
-#   tags       = {
-#     Name     = "Training_Main_RT"
-#   }
-# }
+  tags       = {
+    Name     = "Training_Main_RT"
+  }
+}
 
-# # associate public subnet az1 to "public route table"
-# # terraform aws associate subnet with route table
-# resource "aws_route_table_association" "public_subnet_az1_route_table_association" {
-#   subnet_id           = aws_subnet.public_subnet_az1.id
-#   route_table_id      = aws_route_table.public_route_table.id
-# }
+# associate public subnet az1 to "public route table"
+# terraform aws associate subnet with route table
+resource "aws_route_table_association" "public_subnet_az1_route_table_association" {
+  subnet_id           = aws_subnet.public_subnet_az1.id
+  route_table_id      = aws_route_table.public_route_table.id
+}
 
-# # associate public subnet az2 to "public route table"
-# # terraform aws associate subnet with route table
-# resource "aws_route_table_association" "public_subnet_az2_route_table_association" {
-#   subnet_id           = aws_subnet.public_subnet_az2.id
-#   route_table_id      = aws_route_table.public_route_table.id
-# }
+# associate public subnet az2 to "public route table"
+# terraform aws associate subnet with route table
+resource "aws_route_table_association" "public_subnet_az2_route_table_association" {
+  subnet_id           = aws_subnet.public_subnet_az2.id
+  route_table_id      = aws_route_table.public_route_table.id
+}
 
-# # create private app subnet az1
-# # terraform aws create subnet
-# resource "aws_subnet" "private_app_subnet_az1" {
-#   vpc_id                   = aws_vpc.Training_vpc.id
-#   cidr_block               = var.private_app_subnet_az1_cidr
-#   availability_zone        = var.Availability_Zone1
-#   map_public_ip_on_launch  = "false"
+# create private app subnet az1
+# terraform aws create subnet
+resource "aws_subnet" "private_app_subnet_az1" {
+  vpc_id                   = aws_vpc.Training_vpc.id
+  cidr_block               = var.private_app_subnet_az1_cidr
+  availability_zone        = var.Availability_Zone1
+  map_public_ip_on_launch  = "false"
 
-#   tags      = {
-#     Name    = "Training_Main_PrivAppSub1"
-#   }
-# }
+  tags      = {
+    Name    = "Training_Main_PrivAppSub1"
+  }
+}
 
-# # create private app subnet az2
-# # terraform aws create subnet
-# resource "aws_subnet" "private_app_subnet_az2" {
-#   vpc_id                   = aws_vpc.Training_vpc.id
-#   cidr_block               = var.private_app_subnet_az2_cidr
-#   availability_zone        = var.Availability_Zone2
-#   map_public_ip_on_launch  = "false"
+# create private app subnet az2
+# terraform aws create subnet
+resource "aws_subnet" "private_app_subnet_az2" {
+  vpc_id                   = aws_vpc.Training_vpc.id
+  cidr_block               = var.private_app_subnet_az2_cidr
+  availability_zone        = var.Availability_Zone2
+  map_public_ip_on_launch  = "false"
 
-#   tags      = {
-#     Name    = "Training_Main_PrivAppSub2"
-#   }
-# }
+  tags      = {
+    Name    = "Training_Main_PrivAppSub2"
+  }
+}
 
-# # create private data subnet az1
-# # terraform aws create subnet
-# resource "aws_subnet" "private_data_subnet_az1" {
-#   vpc_id                   = aws_vpc.Training_vpc.id
-#   cidr_block               = var.private_data_subnet_az1_cidr
-#   availability_zone        = var.Availability_Zone1
-#   map_public_ip_on_launch  = "false"
+# create private data subnet az1
+# terraform aws create subnet
+resource "aws_subnet" "private_data_subnet_az1" {
+  vpc_id                   = aws_vpc.Training_vpc.id
+  cidr_block               = var.private_data_subnet_az1_cidr
+  availability_zone        = var.Availability_Zone1
+  map_public_ip_on_launch  = "false"
 
-#   tags      = {
-#     Name    = "Training_Main_PrivDataSub1"
-#   }
-# }
+  tags      = {
+    Name    = "Training_Main_PrivDataSub1"
+  }
+}
 
-# # create private data subnet az2
-# # terraform aws create subnet
-# resource "aws_subnet" "private_data_subnet_az2" {
-#   vpc_id                   = aws_vpc.Training_vpc.id
-#   cidr_block               = var.private_data_subnet_az2_cidr
-#   availability_zone        = var.Availability_Zone2
-#   map_public_ip_on_launch  = "false"
+# create private data subnet az2
+# terraform aws create subnet
+resource "aws_subnet" "private_data_subnet_az2" {
+  vpc_id                   = aws_vpc.Training_vpc.id
+  cidr_block               = var.private_data_subnet_az2_cidr
+  availability_zone        = var.Availability_Zone2
+  map_public_ip_on_launch  = "false"
 
-#   tags      = {
-#     Name    = "Training_Main_PrivDataSub2"
-#   }
-# }
+  tags      = {
+    Name    = "Training_Main_PrivDataSub2"
+  }
+}
